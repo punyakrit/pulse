cf637c5c3b86beb446fcd16a37af5f647ada3918
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-23 02:07:38 +0530

    prisma schema shadcn

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,35 @@
+import { getUser } from '@/lib/actions/User'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import { prisma } from '@/lib/prisma/db'
 
 interface layoutProps {
     children: React.ReactNode
 }
 
-function layout({children}: layoutProps) {
+async function layout({children}: layoutProps) {
+
+  const user = await getUser()
+  if(!user) {
+    redirect('/')
+  }
+
+  const userExists = await prisma.user.findFirst({
+    where:{
+      id: user.id
+    }
+  })
+
+  if(!userExists) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email as string,
+        name: user.given_name,
+      }
+    })
+  }
+
   return (
     <div>
         {children}
